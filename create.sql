0d38b86446700cb757ffd0f9bafa4a0966c7815e
marosimosi <marosimosi@gmail.com>
2022-12-15 14:27:09 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,94 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "Client" (
+	"ssn"	integer NOT NULL,
+	"email"	string NOT NULL,
+	"telephone"	integer NOT NULL,
+	"address"	string NOT NULL,
+	PRIMARY KEY("ssn")
+);
+CREATE TABLE IF NOT EXISTS "Agency" (
+	"name"	string NOT NULL,
+	"web_page"	string NOT NULL,
+	"commision"	float NOT NULL,
+	"agency_ssn"	integer NOT NULL,
+	PRIMARY KEY("agency_ssn"),
+	FOREIGN KEY ("agency_ssn") REFERENCES "Client"("ssn")
+);
+CREATE TABLE IF NOT EXISTS "Booking" (
+	"booking_id"	integer NOT NULL,
+	"price"	float NOT NULL,
+	"arrival"	date NOT NULL,
+	"departure"	date NOT NULL,
+	"downpayment"	float NOT NULL,
+	"paid_amount"	float NOT NULL DEFAULT 0,
+	"dp_due_date"	date NOT NULL,
+	"pay_method"	string NOT NULL,
+	"children"	integer NOT NULL DEFAULT 0,
+	"adults"	integer NOT NULL,
+	"ssn"	integer NOT NULL,
+	PRIMARY KEY("booking_id" AUTOINCREMENT),
+	FOREIGN KEY ("ssn") REFERENCES "Client"("ssn")
+);
+CREATE TABLE IF NOT EXISTS "Books" (
+	"booking_id"	integer NOT NULL,
+	"type_name"	string NOT NULL,
+	"booking_date"	date NOT NULL,
+	PRIMARY KEY("booking_id","type_name"),
+	FOREIGN KEY ("booking_id") REFERENCES "Booking"("booking_id"),
+	FOREIGN KEY ("type_name") REFERENCES "Type"("type_name")
+);
+CREATE TABLE IF NOT EXISTS "Fills" (
+	"booking_id"	integer NOT NULL,
+	"room_id"	integer NOT NULL,
+	"check_in"	date NOT NULL,
+	"check_out"	date NOT NULL,
+	PRIMARY KEY("booking_id","room_id"),
+	FOREIGN KEY ("booking_id") REFERENCES "Booking"("booking_id"),
+	FOREIGN KEY ("room_id") REFERENCES "Room"("room_id")
+);
+CREATE TABLE IF NOT EXISTS "Capacity" (
+	"number"	integer NOT NULL,
+	PRIMARY KEY("number")
+);
+CREATE TABLE IF NOT EXISTS "Individual" (
+	"Fname"	string NOT NULL,
+	"Lname"	string NOT NULL,
+	"Bdate"	date NOT NULL,
+	"individual_ssn"	integer NOT NULL,
+	PRIMARY KEY("individual_ssn"),
+	FOREIGN KEY ("individual_ssn") REFERENCES "Client"("ssn")
+);
+CREATE TABLE IF NOT EXISTS "Rate" (
+	"rate_id"	integer NOT NULL,
+	"mid_season"	float NOT NULL,
+	"low_season"	float NOT NULL,
+	"high_season"	float NOT NULL,
+	PRIMARY KEY("rate_id")
+);
+CREATE TABLE IF NOT EXISTS "Review" (
+	"review_id"	integer NOT NULL,
+	"text"	text,
+	"date"	date NOT NULL,
+	"score"	integer NOT NULL,
+	"ssn"	integer NOT NULL,
+	"type_name"	string NOT NULL,
+	PRIMARY KEY("review_id" AUTOINCREMENT),
+	FOREIGN KEY ("ssn") REFERENCES "Client"("ssn"),
+	FOREIGN KEY ("type_name") REFERENCES "Type"("type_name")
+);
+CREATE TABLE IF NOT EXISTS "Room" (
+	"room_id"	integer NOT NULL,
+	"floor"	integer NOT NULL,
+	"type_name"	string NOT NULL,
+	PRIMARY KEY("room_id" AUTOINCREMENT),
+	FOREIGN KEY ("type_name") REFERENCES "Type"("type_name")
+);
+CREATE TABLE IF NOT EXISTS "Type" (
+	"type_name"	string NOT NULL,
+	"rate_id"	integer NOT NULL,
+	"capacity_num"	integer NOT NULL,
+	PRIMARY KEY("rate_id" AUTOINCREMENT),
+	FOREIGN KEY ("rate_id") REFERENCES "Rate"("rate_id"),
+	FOREIGN KEY ("capacity_num") REFERENCES "Capacity"("number")
+);
+COMMIT;
