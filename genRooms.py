5e796d3f1f024fd71c3a6ded7329644a5c998531
marosimosi <marosimosi@gmail.com>
2023-01-02 02:19:12 +0200

    Update genRooms.py

--- genRooms.py
+++ genRooms.py
@@ -1,31 +1,21 @@
 import sqlite3
 import pandas as pd
 
-#generate Type, Rate, Room
+#generate Type,Room
 
 typedata = pd.read_excel("type.xlsx")
-ratedata = pd.read_excel("rate.xlsx")
 roomdata = pd.read_excel("room.xlsx")
 with sqlite3.connect("database.db") as conn:
     cursor = conn.cursor()
 
     for _, row in typedata.iterrows():
         type_name = row["type_name"]
-        capacity = row["capacity"]
-        rate_id = row["rate_id"]
-        cursor.execute(
-            f"""INSERT or REPLACE INTO TYPE
-        VALUES (?,?,?);""", (type_name, rate_id, capacity)
-        )
-        
-    for _, row in ratedata.iterrows():
-        rate_id = int(row["rate_id"])
         low_season = float(row["low"])
         mid_season = float(row["mid"])
         high_season = float(row["high"])
         cursor.execute(
-            f"""INSERT or REPLACE INTO RATE
-        VALUES (?,?,?,?);""", (rate_id, mid_season, low_season, high_season)
+            f"""INSERT or REPLACE INTO TYPE
+        VALUES (?,?,?,?);""", (type_name, low_season, mid_season, high_season)
         )
 
     for _, row in roomdata.iterrows():
