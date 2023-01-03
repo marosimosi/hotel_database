3c3531f607dcd4fa7dace17bffd36d33b7bc97e7
Athena Pnevmatikaki <a.pnevmatikaki@gmail.com>
2023-01-03 20:25:29 +0200

    Update admin_menu.py

--- admin_menu.py
+++ admin_menu.py
@@ -1,26 +1,40 @@
-from admin_sqlite import DB_Connection
+from admin_sqlite import DB_connection
 from inputs import Inputs
+from datetime import date
 
 class Admin:
     # Initialize the admin menu
     def __init__(self, db_path) -> None:
-        self.db = DB_Connection(db_path)
+        self.db = DB_connection(db_path)
         while True:
             self.option = self.login()
-            if self.option == 1:
+            if self.option == "1":
                 print("Πάτησες το 1!")
-            elif self.option == 2:
+            elif self.option == "2":
                 print("Πάτησες το 2!")
-            elif self.option == -1:
+            elif self.option == "3":
+                self.check_in()
+            elif self.option == "-1":
                 return
 
     # Menu to get the option from the admin
     def login(self):
-        login = Inputs.input_option(
-            "Press 1 to prtint the names of the clients\
-            \nPress 2 ...\n", "Not a valid option.",
-            [1, 2, -1])
+        login = Inputs.input_method(
+            "\n1: Show all the bookings concerning a specific time period.\
+            \n2: Show the 5 reviews with the worst scores and the rooms they concern.\
+            \n3: Check in.\
+            \n4: Check out.\
+            \n-1: Exit\n", "Not a valid option.",
+            ["1", "2", "3", "4", "-1"])
         return login
+
+    # Function to check in a client
+    def check_in(self):
+        booking_id = input("Enter booking id:")
+        room_id = input("Enter room id:")
+        check_in_date = date.today()
+        check_out_date = 'NULL'
+        self.db.inster_fills(booking_id, room_id, check_in_date, check_out_date)
 
 
 if __name__ == "__main__":
