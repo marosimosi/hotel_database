374ec13788d2b358fe76165a364b0e4a9ca36839
Athena Pnevmatikaki <a.pnevmatikaki@gmail.com>
2023-01-03 20:09:50 +0200

    Update admin_sqlite.py

--- admin_sqlite.py
+++ admin_sqlite.py
@@ -5,3 +5,11 @@ from inputs import Inputs
 class DB_connection:
     def __init__(self, db_path) -> None:
         self.conn = sqlite3.connect(db_path)
+
+    # Inserts new entry in Fills table
+    def inster_fills(self, booking_id, room_id, check_in, check_out):
+        cursor = self.conn.cursor()
+        sql = """INSERT INTO FILLS(booking_id, room_id, check_in, check_out) 
+        VALUES(?,?,?,?)"""
+        cursor.execute(sql, (booking_id, room_id, check_in, check_out))
+        self.conn.commit()
