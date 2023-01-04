43ccd5ac50b8f6f26a1ac6d131e6f638d43b9d2a
marosimosi <marosimosi@gmail.com>
2023-01-04 02:57:19 +0200

    Update client_sqlite.py

--- client_sqlite.py
+++ client_sqlite.py
@@ -6,7 +6,19 @@ class DB_Connection:
         self.connection = sqlite3.connect(path_name)
     
     def check_availability(self, type_name, from_date, to_date):
-        return 0
+        return []
 
     def read_reviews(self, type_name):
+        return
+
+    def calc_price(self, room, from_date, to_date):
+        return 0
+
+    def is_new(self, ssn):
+        return True
+
+    def new_client(self, ssn, email, tel, address, fname, lname, bdate):
+        return
+
+    def book(self, price, from_date, to_date, pay_method, adults, children, ssn):
         return
